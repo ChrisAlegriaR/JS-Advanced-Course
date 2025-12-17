949cceba82c43c22e16dee52f82fc50753c3e9b7
Chris Alegria <christian.alegriar@gmail.com>
2025-12-17 00:01:46 -0600

    Update: Test

--- Exercises/Excercise_2/scripts/script.js
+++ Exercises/Excercise_2/scripts/script.js
@@ -1,25 +1,7 @@
-process.stdin.on("data", (data) => {
-    const input = String(data);
-    input.trim();
-    input.toLocaleLowerCase;
+for (const key in object) {
+    if (!Object.hasOwn(object, key)) continue;
     
-    let arrayFrase = input.split(' ');
-    let counter = {};
-
-    arrayFrase.forEach(element => {
-        if(element.length > 3){
-            if(counter[element]){
-                counter[element]++;
-            }else{
-                counter[element] = 1;
-            }            
-        }
-    });
-
-    // console.log(counter)
-    // console.log(`La palabra que mas se repite es: `, counter.hola )
-});
-
-
-
-
+    const element = object[key];
+    
+    
+}
