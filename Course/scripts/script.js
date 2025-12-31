765512b62665c6e70715f833d12d6817eb219be0
Chris Alegria <christian.alegriar@gmail.com>
2025-12-31 16:15:35 -0600

    Add/Finished: Teoric Informacion About OOP

---
+++ Course/scripts/script.js
@@ -0,0 +1,35 @@
+// ^JavaScript Advanced Course
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// ~¿Que es un paradigma en programacion?
+// Un paradigma en programacion es un enfoque o estilo que define como se estructuran y escriben los programas. Cada paradigma estbalece reglas sobre como los desarrolladores pueden rear, organizar, y manipular el codigo para resolver problemas.
+// Algunos ejemplos incluyen la programacion imperativa, orientada a objetos, funcional y logica, cada una con sus propias caracteristicas y principios.
+// Comprender estos paradigmas es crucial para seleccionar el enfoque mas adecuado al resolver problemas y diseñar sistemas de software ficientes y mantenibles.
+
+// ~Programación orientada a objetos (OOP)
+// Es un paradigma de programacion que organiza el codigo en "objetos", cada uno con sus propios datos y comportamientos.
+// Se centra en conceptos como la abstraccion, encapsulamiento, herencia y polimorfismo para facilitar el desarrollo de software modular, reutilizable y facil de mantener.
+// En OOP, os objetos interactuan entre si mediante "mensajes", lo que permite una representacion mas cercana de los conceptos del mundo real y una mejor gestion de la complejidad en los programas.
+
+// ~Programación orientada a objetos (OOP)
+// ?Clases: Son la piedra angular de na OOP actuando como plantillas para la creacion de objetos. Definen las propiedades (atributos) y comportamientos (metodos) que caracterisan a los objetos de un tipo especifico. Al encapsular datos y funcionealidades relacionadas, permiten la reutilizacion de codigo, la organizacion modular y la abstraccion.
+
+// ?Atributos: Son las caracteristicas o datos que describen el estado de un objeto en un contexto determinado. Estos pueden representar desde valores simples como numeros o cadenas de texto, hasta estructuras mas complejas como objetos u otro tipo de datos. Los atributos definen las propiedades de un objeto con los cuales se gestiona el estado del mismo.
+
+// ?Metodos: Son bloques de codigo que encapsulan una serie de acciones o comportamientos especificos asociados a un objeto. Estos permiten realizar operaciones sobre los datos de un objeto, modificar su estado interno y responder a eventos dentro de un programa. Los metodos metodos pueden acceder a los atributos de un objeto.
+
+// ?Objetos: Los objetos son instancias especificas de una clase que encapsulan datos y comportamientos relacionados. Los objetos permiten modelar entidades del mundo real de manera modular y reutilizable, facilitando la organizacion y el diseño de sistemas de software eficientes y mantenibles.
+
+// ~Caracteristicas de la OOP
+// ?Abstraccion: Permite representar entidades del mundo real como objetos con caracteristicas y comportamientos relevantes para el problema que se esta resolviendo. Esto simplifica la complejidad del sistema al enfocarse en aspectos esenciales y proporcionar una forma de modelar conceptos del mundo real en el codigo.
+
+// ?Encapsulamiento: Los detalles internos de un objeto deben estar ocultos fuera de su definicion y solo deben ser accesibles a traves de una interfaz claramente definida. Esto promueve la seguridad y la integridad de los datos al prevenir accesos no autorizados y facilita el mantemiento del codigo.
+
+// ?Herencia: Permite que una clase (subclase) herede atributos y metodos de otra clase (superclase), lo que promueve la reutlizacion del codigo y la organizacion jerarquica de las clases. Esto facilita la creacion de nuevas clases que extienden el comportamiento de clases que extienden el comportamiento de clases existentes, permitiendo una mayor flexibilidad y modularidad en el diseño del software.
+
+// ?Polimorfismo: Se refiere a la capacidad de objetos de diferentes clases de responder al mismo mensaje de manera diferente. En otras palabras, un mismo metodo o mensaje puede reproducir diferentes resultados segun el tipo de objeto que lo reciba. Esto permite escribir codigo mas generico y reutilizable.
+
+// ~Versiones y detras de escena
+// Antes de ES6, javascript no tenia clases formales. Se usaban funciones constructoras y prototipos para simular la OOP. Con ES6, se introdujo la sintaxis class, aunque internamente sigue usando prototipos.
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
